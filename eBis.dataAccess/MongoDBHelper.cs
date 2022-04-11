0e524c036b7b882b11d1a1a8c1cc40f93f5bc2df
Anaïs TORRES <anais@altazion.com>
2022-04-11 15:24:19 +0200

    push common

--- eBis.dataAccess/MongoDBHelper.cs
+++ eBis.dataAccess/MongoDBHelper.cs
@@ -9,5 +9,4 @@ public static class MongoDBHelper
     private static string _connectionString = "mongodb+srv://eBisOperator:HARDpassword@cluster0.vzvci.mongodb.net/eBis?retryWrites=true&w=majority";
     public static MongoClient MongoClient = new MongoClient(_connectionString); //connexion à mongo
     public static IMongoDatabase eBisMongoBase = MongoClient.GetDatabase("eBis"); //connexion BDD
-
 }
