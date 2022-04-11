4b9f97426fb013994c7bf30ce25ea5d55a78c3d0
danyrose <danyrose1995@gmail.com>
2022-04-11 14:56:05 +0200

    conexxion bdd

---
+++ eBis.dataAccess/MongoDBHelper.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public static class MongoDBHelper
+{
+    private static string _connectionString = "mongodb+srv://eBisOperator:HARDpassword@cluster0.vzvci.mongodb.net/eBis?retryWrites=true&w=majority";
+    public static MongoClient MongoClient = new MongoClient(_connectionString); //connexion à mongo
+    public static IMongoDatabase eBisMongoBase = MongoClient.GetDatabase("eBis"); //connexion BDD
+
+}
