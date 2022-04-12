307305f92d8487f146ae796ccd26c8239a787eff
Anaïs TORRES <anais@altazion.com>
2022-04-12 09:13:26 +0200

    méthodes Secteur

--- eBis.dataAccess/SecteurMethod.cs
+++ eBis.dataAccess/SecteurMethod.cs
@@ -4,17 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using eBis.dataAccess;
+using eBis.common;
+using eBis.common.Models;
+
 
 namespace eBis.dataAccess
 {
     public class SecteurMethod
     {
-        private IMongoCollection<SecteurMethod> _secteur; // accès à la collection
+        private IMongoCollection<Secteur> _secteur; // accès à la collection
 
         public SecteurMethod()
         {
-            _secteur = MongoDBHelper.eBisMongoBase.GetCollection<SecteurMethod>("secteur"); // connexion à la collection
+            _secteur = MongoDBHelper.eBisMongoBase.GetCollection<Secteur>("secteur"); // connexion à la collection
+        }
 
+        // Récupère la liste des secteurs 
+        public List<Secteur> getAllSecteur()
+        {
+            List<Secteur> sctr = _secteur.Find(s => true).ToList();
+            return sctr;
         }
     }
 }
